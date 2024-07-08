c41412d1597f51652b330739cc3b4273df19be45
jmzqq <joaomwguel@gmail.com>
2024-07-08 15:58:53 -0300

    codigo

---
+++ codigo_calculadora/Teste_Xunit/Calculadora.cs
@@ -0,0 +1,27 @@
+﻿namespace Teste_Xunit
+{
+    public static class Calculadora
+    {
+        public static double Somar(double x, double y)
+        {
+            return x + y;
+        }
+
+        public static double Subtrair(double x, double y)
+        {
+            return x - y;
+        }
+
+        public static double Mutiplicar(double x, double y)
+        {
+            return x * y;
+        }
+
+        public static double Dividir(double x, double y)
+        {
+            return x / y;
+        }
+        
+        
+    }
+}
---
+++ codigo_calculadora/Teste_Xunit/CalculadoraTeste.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Teste_Xunit
+{
+    public class CalculadoraTeste
+    {
+        [Fact]
+        public void TesteSomar()
+        {
+            double resultado = Calculadora.Somar(77, 33);
+            Assert.Equal(110,resultado);
+        }
+
+        [Fact]
+        public void TesteSubtrair()
+        {
+            double resultado = Calculadora.Subtrair(10, 7);
+            Assert.Equal(3, resultado);
+        }
+
+        [Fact]
+        public void TesteMutiplicação()
+        {
+            double resultado = Calculadora.Mutiplicar(4, 25);
+            Assert.Equal(100, resultado);
+        }
+
+        [Fact]
+        public void TesteDivisão()
+        {
+            double resultado = Calculadora.Dividir(81, 9);
+            Assert.Equal(9, resultado);
+        }
+
+        [Fact]
+        public void DivisãoPorZero()
+        {
+            double resultado = Calculadora.Dividir(10, 0);
+            Assert.Equal(double.PositiveInfinity, resultado);
+        }
+    }
+}
